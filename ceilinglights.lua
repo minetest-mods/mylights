5a4c40c82437e67e329d6fae8d994f2a3c01ba30
Don <serfdon@gmail.com>
2025-04-01 10:48:36 -0700

    Added More Lights

--- ceilinglights.lua
+++ ceilinglights.lua
@@ -16,7 +16,7 @@ minetest.register_node("mylights:ceiling_light_30_"..color, {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	light_source = 5,
-	groups = {snappy = 1},
+	groups = {snappy = 1, not_in_creative_inventory=1},
 	node_box = {
 		type = "fixed",
 		fixed = {
@@ -118,7 +118,7 @@ minetest.register_node("mylights:ceiling_light_lg_30_"..color, {
 	paramtype = "light",
 	paramtype2 = "facedir",
 	light_source = 5,
-	groups = {snappy = 1},
+	groups = {snappy = 1, not_in_creative_inventory=1},
 	node_box = {
 		type = "fixed",
 		fixed = {
@@ -227,4 +227,498 @@ minetest.register_craft({
 			{'','dye:'..color,''}
 			}
 })
+
 end
+
+--Round Ceiling Light
+minetest.register_node("mylights:ceiling_light_round_30", {
+	description = "Larger Round Ceiling Light 30w",
+	tiles = {"mylights_white.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 5,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, 0.5, 0.25, 0.5, 0.3125, -0.25},
+			{-0.4375, 0.3125, -0.3125, 0.4375, 0.5, 0.3125},
+			{-0.375, 0.3125, -0.375, 0.375, 0.5, 0.375},
+			{-0.3125, 0.3125, -0.4375, 0.3125, 0.5, 0.4375},
+			{-0.25, 0.3125, -0.5, 0.25, 0.5, 0.5}, 
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_round_30 1",
+		recipe = {
+			{'','default:glass',''},
+			{'default:glass','mylights:lightbulb30','default:glass'},
+			{'','default:glass',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_round_60", {
+	description = "Larger Round Ceiling Light 60w",
+	tiles = {"mylights_white.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 8,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, 0.5, 0.25, 0.5, 0.3125, -0.25},
+			{-0.4375, 0.3125, -0.3125, 0.4375, 0.5, 0.3125},
+			{-0.375, 0.3125, -0.375, 0.375, 0.5, 0.375},
+			{-0.3125, 0.3125, -0.4375, 0.3125, 0.5, 0.4375},
+			{-0.25, 0.3125, -0.5, 0.25, 0.5, 0.5},  
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_round_60 1",
+		recipe = {
+			{'','default:glass',''},
+			{'default:glass','mylights:lightbulb60','default:glass'},
+			{'','default:glass',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_round_90", {
+	description = "Larger Round Ceiling Light 90w",
+	tiles = {"mylights_white.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 11,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, 0.5, 0.25, 0.5, 0.3125, -0.25},
+			{-0.4375, 0.3125, -0.3125, 0.4375, 0.5, 0.3125},
+			{-0.375, 0.3125, -0.375, 0.375, 0.5, 0.375},
+			{-0.3125, 0.3125, -0.4375, 0.3125, 0.5, 0.4375},
+			{-0.25, 0.3125, -0.5, 0.25, 0.5, 0.5},  
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_round_90 1",
+		recipe = {
+			{'','default:glass',''},
+			{'default:glass','mylights:lightbulb90','default:glass'},
+			{'','default:glass',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_round_120", {
+	description = "Larger Round Ceiling Light 120w",
+	tiles = {"mylights_white.png"},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 14,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.5, 0.5, 0.25, 0.5, 0.3125, -0.25},
+			{-0.4375, 0.3125, -0.3125, 0.4375, 0.5, 0.3125},
+			{-0.375, 0.3125, -0.375, 0.375, 0.5, 0.375},
+			{-0.3125, 0.3125, -0.4375, 0.3125, 0.5, 0.4375},
+			{-0.25, 0.3125, -0.5, 0.25, 0.5, 0.5},  
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_round_120 1",
+		recipe = {
+			{'','default:glass',''},
+			{'default:glass','mylights:lightbulb120','default:glass'},
+			{'','default:glass',''}
+			}
+})
+
+--Fluorescent Light
+minetest.register_node("mylights:ceiling_light_fluorescent_30", {
+	description = "Fluorescent Ceiling Light 30w",
+	tiles = {
+			"mylights_greybg.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_fluorescent_sides.png^[transformR90",
+			"mylights_fluorescent_sides.png^[transformR90",
+			"mylights_white.png",
+			"mylights_white.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 5,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, 0.375, -0.5, 0.1875, 0.5, 0.5}, 
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_fluorescent_30 1",
+		recipe = {
+			{'','',''},
+			{'default:glass','mylights:lightbulb30','default:glass'},
+			{'','',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_fluorescent_60", {
+	description = "Fluorescent Ceiling Light 60w",
+	tiles = {
+			"mylights_greybg.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_white.png",
+			"mylights_white.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 8,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, 0.375, -0.5, 0.1875, 0.5, 0.5},  
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_fluorescent_60 1",
+		recipe = {
+			{'','',''},
+			{'default:glass','mylights:lightbulb60','default:glass'},
+			{'','',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_fluorescent_90", {
+	description = "Fluorescent Ceiling Light 90w",
+	tiles = {
+			"mylights_greybg.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_white.png",
+			"mylights_white.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 11,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, 0.375, -0.5, 0.1875, 0.5, 0.5},  
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_fluorescent_90 1",
+		recipe = {
+			{'','',''},
+			{'default:glass','mylights:lightbulb90','default:glass'},
+			{'','',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_fluorescent_120", {
+	description = "Fluorescent Ceiling Light 120w",
+	tiles = {
+			"mylights_greybg.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_fluorescent_sides.png",
+			"mylights_white.png",
+			"mylights_white.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 14,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, 0.375, -0.5, 0.1875, 0.5, 0.5},  
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_fluorescent_120 1",
+		recipe = {
+			{'','',''},
+			{'default:glass','mylights:lightbulb120','default:glass'},
+			{'','',''}
+			}
+})
+
+
+--Hanging Light
+minetest.register_node("mylights:ceiling_light_hanging_30", {
+	description = "Hanging Ceiling Light 30w",
+	tiles = {
+			"mylights_white.png",
+			"mylights_white.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 5,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0.4375, -0.125, 0.125, 0.5, 0.125},
+			{-0.0625, -0.0625, -0.0625, 0.0625, 0.4375, 0.0625},
+			{-0.0625, -0.3125, -0.125, 0.0625, -0.0625, 0.125},
+			{-0.125, -0.3125, -0.0625, 0.125, -0.0625, 0.0625}, 
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_hanging_30 1",
+		recipe = {
+			{'','default:copper_ingot',''},
+			{'','default:glass',''},
+			{'','mylights:lightbulb30',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_hanging_60", {
+	description = "Hanging Ceiling Light 60w",
+	tiles = {
+			"mylights_white.png",
+			"mylights_white.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 8,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0.4375, -0.125, 0.125, 0.5, 0.125},
+			{-0.0625, -0.0625, -0.0625, 0.0625, 0.4375, 0.0625},
+			{-0.0625, -0.3125, -0.125, 0.0625, -0.0625, 0.125},
+			{-0.125, -0.3125, -0.0625, 0.125, -0.0625, 0.0625},   
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_hanging_60 1",
+		recipe = {
+			{'','default:cooper_ingot',''},
+			{'','default:glass',''},
+			{'','mylights:lightbulb60',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_hanging_90", {
+	description = "Hanging Ceiling Light 90w",
+	tiles = {
+			"mylights_white.png",
+			"mylights_white.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 11,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0.4375, -0.125, 0.125, 0.5, 0.125},
+			{-0.0625, -0.0625, -0.0625, 0.0625, 0.4375, 0.0625},
+			{-0.0625, -0.3125, -0.125, 0.0625, -0.0625, 0.125},
+			{-0.125, -0.3125, -0.0625, 0.125, -0.0625, 0.0625},  
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_hanging_90 1",
+		recipe = {
+			{'','default:copper_ingot',''},
+			{'','default:glass',''},
+			{'','mylights:lightbulb90',''}
+			}
+})
+minetest.register_node("mylights:ceiling_light_hanging_120", {
+	description = "Hanging Ceiling Light 120w",
+	tiles = {
+			"mylights_white.png",
+			"mylights_white.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			"mylights_hanging_light_sides.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 14,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0.4375, -0.125, 0.125, 0.5, 0.125},
+			{-0.0625, -0.0625, -0.0625, 0.0625, 0.4375, 0.0625},
+			{-0.0625, -0.3125, -0.125, 0.0625, -0.0625, 0.125},
+			{-0.125, -0.3125, -0.0625, 0.125, -0.0625, 0.0625},  
+		}
+	},
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_hanging_120 1",
+		recipe = {
+			{'','default:copper_ingot',''},
+			{'','default:glass',''},
+			{'','mylights:lightbulb120',''}
+			}
+})
+
+--Cube Light
+minetest.register_node("mylights:ceiling_light_cube_30", {
+	description = "Cube Light 30w",
+	tiles = {
+			"mylights_cube_light_top.png",
+			"mylights_cube_light_top.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 5,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.5, -0.375, 0.375, 0.25, 0.375}, 
+		}
+	},
+	on_place = minetest.rotate_node,
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_cube_30 1",
+		recipe = {
+			{'default:glass','','default:glass'},
+			{'','mylights:lightbulb30',''},
+			{'default:glass','','default:glass'}
+			}
+})
+minetest.register_node("mylights:ceiling_light_cube_60", {
+	description = "Cube Light 60w",
+	tiles = {
+			"mylights_cube_light_top.png",
+			"mylights_cube_light_top.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 8,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.5, -0.375, 0.375, 0.25, 0.375},   
+		}
+	},
+	on_place = minetest.rotate_node,
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_cube_60 1",
+		recipe = {
+			{'default:glass','','default:glass'},
+			{'','mylights:lightbulb60',''},
+			{'default:glass','','default:glass'}
+			}
+})
+minetest.register_node("mylights:ceiling_light_cube_90", {
+	description = "Cube Light 90w",
+	tiles = {
+			"mylights_cube_light_top.png",
+			"mylights_cube_light_top.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 11,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.5, -0.375, 0.375, 0.25, 0.375}, 
+		}
+	},
+	on_place = minetest.rotate_node,
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_cube_90 1",
+		recipe = {
+			{'default:glass','','default:glass'},
+			{'','mylights:lightbulb90',''},
+			{'default:glass','','default:glass'}
+			}
+})
+minetest.register_node("mylights:ceiling_light_cube_120", {
+	description = "Cube Light 120w",
+	tiles = {
+			"mylights_cube_light_top.png",
+			"mylights_cube_light_top.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			"mylights_cube_light_sides.png",
+			},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	light_source = 14,
+	groups = {snappy = 1, not_in_creative_inventory=1},
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.5, -0.375, 0.375, 0.25, 0.375},  
+		}
+	},
+	on_place = minetest.rotate_node,
+})
+minetest.register_craft({
+		output = "mylights:ceiling_light_cube_120 1",
+		recipe = {
+			{'default:glass','','default:glass'},
+			{'','mylights:lightbulb120',''},
+			{'default:glass','','default:glass'}
+			}
+})
--- ws.lua
+++ ws.lua
@@ -1,4 +1,4 @@
-
+--Wall Sconce 1
 minetest.register_node("mylights:ws_30", {
 	description = "Wall Sconce 30w",
 	tiles = {
@@ -188,9 +188,410 @@ minetest.register_craft({
 			}
 })
 
+--Wall Sconce 2
+minetest.register_node("mylights:ws2_30", {
+	description = "Wall Sconce 2 30w",
+	tiles = {
+		"mylights_ws2_sides.png^[transformR90",
+		"mylights_ws2_sides.png^[transformR270",
+		"mylights_ws2_sides.png",
+		"mylights_ws2_sides.png^[transformR180",
+		"mylights_greybg.png",
+		"mylights_ws2_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1},
+	light_source = 5,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.25, 0.4375, 0.25, 0.25, 0.5},
+			{-0.1875, -0.1875, 0.3125, 0.1875, 0.1875, 0.4375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws2_30 2",
+		recipe = {
+			{'','mylights:lightbulb30',''},
+			{'','default:tin_ingot',''},
+			{'','','default:tin_ingot'}
+			}
+})
+minetest.register_node("mylights:ws2_60", {
+	description = "Wall Sconce 2 60w",
+	tiles = {
+		"mylights_ws2_sides.png^[transformR90",
+		"mylights_ws2_sides.png^[transformR270",
+		"mylights_ws2_sides.png",
+		"mylights_ws2_sides.png^[transformR180",
+		"mylights_greybg.png",
+		"mylights_ws2_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 8,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.25, 0.4375, 0.25, 0.25, 0.5},
+			{-0.1875, -0.1875, 0.3125, 0.1875, 0.1875, 0.4375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws2_60 2",
+		recipe = {
+			{'','mylights:lightbulb60',''},
+			{'','default:tin_ingot',''},
+			{'','','default:tin_ingot'}
+			}
+})
+minetest.register_node("mylights:ws2_90", {
+	description = "Wall Sconce 2 90w",
+	tiles = {
+		"mylights_ws2_sides.png^[transformR90",
+		"mylights_ws2_sides.png^[transformR270",
+		"mylights_ws2_sides.png",
+		"mylights_ws2_sides.png^[transformR180",
+		"mylights_greybg.png",
+		"mylights_ws2_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 11,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.25, 0.4375, 0.25, 0.25, 0.5},
+			{-0.1875, -0.1875, 0.3125, 0.1875, 0.1875, 0.4375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws2_90 2",
+		recipe = {
+			{'','mylights:lightbulb90',''},
+			{'','default:tin_ingot',''},
+			{'','','default:tin_ingot'}
+			}
+})
+minetest.register_node("mylights:ws2_120", {
+	description = "Wall Sconce 2 120w",
+	tiles = {
+		"mylights_ws2_sides.png^[transformR90",
+		"mylights_ws2_sides.png^[transformR270",
+		"mylights_ws2_sides.png",
+		"mylights_ws2_sides.png^[transformR180",
+		"mylights_greybg.png",
+		"mylights_ws2_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 14,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.25, 0.4375, 0.25, 0.25, 0.5},
+			{-0.1875, -0.1875, 0.3125, 0.1875, 0.1875, 0.4375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws2_120 2",
+		recipe = {
+			{'','mylights:lightbulb120',''},
+			{'','default:tin_ingot',''},
+			{'','','default:tin_ingot'}
+			}
+})
+
+
+--Wall Sconce 3
+minetest.register_node("mylights:ws3_30", {
+	description = "Wall Sconce 3 30w",
+	tiles = {
+		"mylights_ws2_sides.png^[transformR90",
+		"mylights_ws2_sides.png^[transformR270",
+		"mylights_ws2_sides.png",
+		"mylights_ws2_sides.png^[transformR180",
+		"mylights_greybg.png",
+		"mylights_ws3_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1},
+	light_source = 5,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.375, 0.4375, 0.1875, 0.375, 0.5},
+			{-0.125, -0.3125, 0.3125, 0.125, 0.3125, 0.4375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws3_30 2",
+		recipe = {
+			{'','mylights:lightbulb30',''},
+			{'','default:steel_ingot',''},
+			{'','','default:tin_ingot'}
+			}
+})
+minetest.register_node("mylights:ws3_60", {
+	description = "Wall Sconce 3 60w",
+	tiles = {
+		"mylights_ws2_sides.png^[transformR90",
+		"mylights_ws2_sides.png^[transformR270",
+		"mylights_ws2_sides.png",
+		"mylights_ws2_sides.png^[transformR180",
+		"mylights_greybg.png",
+		"mylights_ws3_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 8,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.375, 0.4375, 0.1875, 0.375, 0.5},
+			{-0.125, -0.3125, 0.3125, 0.125, 0.3125, 0.4375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws3_60 2",
+		recipe = {
+			{'','mylights:lightbulb60',''},
+			{'','default:steel_ingot',''},
+			{'','','default:tin_ingot'}
+			}
+})
+minetest.register_node("mylights:ws3_90", {
+	description = "Wall Sconce 3 90w",
+	tiles = {
+		"mylights_ws2_sides.png^[transformR90",
+		"mylights_ws2_sides.png^[transformR270",
+		"mylights_ws2_sides.png",
+		"mylights_ws2_sides.png^[transformR180",
+		"mylights_greybg.png",
+		"mylights_ws3_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 11,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.375, 0.4375, 0.1875, 0.375, 0.5},
+			{-0.125, -0.3125, 0.3125, 0.125, 0.3125, 0.4375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws3_90 2",
+		recipe = {
+			{'','mylights:lightbulb90',''},
+			{'','default:steel_ingot',''},
+			{'','','default:tin_ingot'}
+			}
+})
+minetest.register_node("mylights:ws3_120", {
+	description = "Wall Sconce 3 120w",
+	tiles = {
+		"mylights_ws2_sides.png^[transformR90",
+		"mylights_ws2_sides.png^[transformR270",
+		"mylights_ws2_sides.png",
+		"mylights_ws2_sides.png^[transformR180",
+		"mylights_greybg.png",
+		"mylights_ws3_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 14,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.1875, -0.375, 0.4375, 0.1875, 0.375, 0.5},
+			{-0.125, -0.3125, 0.3125, 0.125, 0.3125, 0.4375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws3_120 2",
+		recipe = {
+			{'','mylights:lightbulb120',''},
+			{'','default:steel_ingot',''},
+			{'','','default:tin_ingot'}
+			}
+})
 
 
 
+--Wall Sconce 4
+minetest.register_node("mylights:ws4_30", {
+	description = "Wall Sconce 4 30w",
+	tiles = {
+		"mylights_ws4_top.png",
+		"mylights_ws4_bottom.png^[transformR180",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1},
+	light_source = 5,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0.125, 0.4375, 0.125, 0.375, 0.5},
+			{-0.0625, -0.0625, 0.25, 0.0625, 0.3125, 0.375},
+			{-0.0625, 0.1875, 0.375, 0.0625, 0.3125, 0.4375},
+			{-0.0625, -0.3125, 0.1875, 0.0625, -0.0625, 0.4375},
+			{-0.125, -0.3125, 0.25, 0.125, -0.0625, 0.375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws4_30 2",
+		recipe = {
+			{'','mylights:lightbulb30',''},
+			{'','default:tin_ingot',''},
+			{'','','default:steel_ingot'}
+			}
+})
+minetest.register_node("mylights:ws4_60", {
+	description = "Wall Sconce 4 60w",
+	tiles = {
+		"mylights_ws4_top.png",
+		"mylights_ws4_bottom.png^[transformR180",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 8,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0.125, 0.4375, 0.125, 0.375, 0.5},
+			{-0.0625, -0.0625, 0.25, 0.0625, 0.3125, 0.375},
+			{-0.0625, 0.1875, 0.375, 0.0625, 0.3125, 0.4375},
+			{-0.0625, -0.3125, 0.1875, 0.0625, -0.0625, 0.4375},
+			{-0.125, -0.3125, 0.25, 0.125, -0.0625, 0.375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws4_60 2",
+		recipe = {
+			{'','mylights:lightbulb60',''},
+			{'','default:tin_ingot',''},
+			{'','','default:steel_ingot'}
+			}
+})
+minetest.register_node("mylights:ws4_90", {
+	description = "Wall Sconce 4 90w",
+	tiles = {
+		"mylights_ws4_top.png",
+		"mylights_ws4_bottom.png^[transformR180",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 11,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0.125, 0.4375, 0.125, 0.375, 0.5},
+			{-0.0625, -0.0625, 0.25, 0.0625, 0.3125, 0.375},
+			{-0.0625, 0.1875, 0.375, 0.0625, 0.3125, 0.4375},
+			{-0.0625, -0.3125, 0.1875, 0.0625, -0.0625, 0.4375},
+			{-0.125, -0.3125, 0.25, 0.125, -0.0625, 0.375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws4_90 2",
+		recipe = {
+			{'','mylights:lightbulb90',''},
+			{'','default:tin_ingot',''},
+			{'','','default:steel_ingot'}
+			}
+})
+minetest.register_node("mylights:ws4_120", {
+	description = "Wall Sconce 4 120w",
+	tiles = {
+		"mylights_ws4_top.png",
+		"mylights_ws4_bottom.png^[transformR180",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png",
+		"mylights_ws4_front.png"
+	},
+	drawtype = "nodebox",
+	paramtype = "light",
+	paramtype2 = "facedir",
+	groups = {cracky=1,snappy = 1,oddly_breakable_by_hand=1, not_in_creative_inventory=1},
+	light_source = 14,
+	node_box = {
+		type = "fixed",
+		fixed = {
+			{-0.125, 0.125, 0.4375, 0.125, 0.375, 0.5},
+			{-0.0625, -0.0625, 0.25, 0.0625, 0.3125, 0.375},
+			{-0.0625, 0.1875, 0.375, 0.0625, 0.3125, 0.4375},
+			{-0.0625, -0.3125, 0.1875, 0.0625, -0.0625, 0.4375},
+			{-0.125, -0.3125, 0.25, 0.125, -0.0625, 0.375},
+		}
+	}
+})
+--craft
+minetest.register_craft({
+		output = "mylights:ws4_120 2",
+		recipe = {
+			{'','mylights:lightbulb120',''},
+			{'','default:tin_ingot',''},
+			{'','','default:steel_ingot'}
+			}
+})
 
 
 
